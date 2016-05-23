e39c17cc5c5735e3e10b5546fed7fe90d9d059ce
Michael Reiniger <m.reiniger@silverorbit.de>
2016-05-23 10:34:31 +0200

    updated Dependencies and own version

--- test/log4js-syslog-appender.js
+++ test/log4js-syslog-appender.js
@@ -57,42 +57,42 @@ describe('log4js-syslog-appender', function () {
 
         it('should log a trace message', function(done){
             logger.trace('trace message');
-            messages[0].should.equal('[TRACE] - trace message');
+            messages[0].should.equal('trace message');
             clearMessages();
             done();
         });
 
         it('should log a debug message', function(done){
             logger.debug('debug message');
-            messages[0].should.equal('[DEBUG] - debug message');
+            messages[0].should.equal('debug message');
             clearMessages();
             done();
         });
 
         it('should log an info message', function(done){
             logger.info('info message');
-            messages[0].should.equal('[INFO] - info message');
+            messages[0].should.equal('info message');
             clearMessages();
             done();
         });
 
         it('should log a warn message', function(done){
             logger.warn('warn message');
-            messages[0].should.equal('[WARN] - warn message');
+            messages[0].should.equal('warn message');
             clearMessages();
             done();
         });
 
         it('should log an error message', function(done){
             logger.error('error message');
-            messages[0].should.equal('[ERROR] - error message');
+            messages[0].should.equal('error message');
             clearMessages();
             done();
         });
 
         it('should log a fatal message', function(done){
             logger.fatal('fatal message');
-            messages[0].should.equal('[FATAL] - fatal message');
+            messages[0].should.equal('fatal message');
             clearMessages();
             done();
         });
